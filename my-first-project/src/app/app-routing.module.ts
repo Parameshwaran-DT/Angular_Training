8db119cb5a321dd75f5cc8fdb785f896458c6ccd
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 <parameshwaran@dorustree.in>
2022-09-07 19:57:43 +0530

    Creating Nested Component, Binding

--- my-first-project/src/app/app-routing.module.ts
+++ my-first-project/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
+  {path:'', component:StudentComponent},
   {path:'home', component:HomeComponent},
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent}
--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `<div>
+                    <h1>{{pageHeader}}</h1> 
+                    <app-student></app-student>                 
+               </div>`
 })
 export class AppComponent {
-  title = 'my-first-project';
+  pageHeader: string = 'Student Details';
 }
