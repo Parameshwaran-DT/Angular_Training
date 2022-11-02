cc4ad76bfaca5d980df866b9bea236c839fc7c48
Parameshwaran-DT <parameshwaran@dorustree.in>
2022-11-02 20:13:56 +0530

    Add routing and install ngrx store devtools

--- NgRx/src/app/app-routing.module.ts
+++ NgRx/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter/counter.component';
+import { HomeComponent } from './home/home.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : HomeComponent},
+  {path: 'counter', component: CounterComponent},
+  {path: 'post', component: PostListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
